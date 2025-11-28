83a781dfa91595b1e76d2cdceedfe1ac8c8dfc36
kaneruda <dskazeeva3@gmail.com>
2025-11-28 16:25:02 +0400

    Initial commit: Add main script with hello world

---
+++ main.py
@@ -0,0 +1,7 @@
+# Это мой первый скрипт на курсе "Введение в инженерную деятельность"
+
+author_name = 'Daria'
+
+print('Hello, World!')
+print(f'I am a future engineer, and my name is {author_name}.')
+print('This project is now on GitHub!')
